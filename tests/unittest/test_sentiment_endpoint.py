f51c6a35d0ddfe6a2b9653eba5ba4579eac4f096
alpaca00 <alpaca00tuha@gmail.com>
2024-10-15 22:56:34 +0200

    Fixed tests to accommodate the addition of the auth decorator

--- tests/unittest/test_sentiment_endpoint.py
+++ tests/unittest/test_sentiment_endpoint.py
@@ -8,7 +8,8 @@ from tests.unittest.conftest import MockSentimentRequest, MockSentimentResponse
 def test_sentiment_analysis_valid_input(client, mock_nltk):
     """Test the sentiment analysis with valid input."""
     response = client.post(
-        "/api/v1/sentiment-analysis", json={"text": MockSentimentRequest().text}
+        "/api/v1/sentiment-analysis",
+        json={"text": MockSentimentRequest().text, "process_text": True},
     )
 
     actual_result = response.get_json()
@@ -31,7 +32,9 @@ def test_sentiment_analysis_valid_input(client, mock_nltk):
 )
 def test_sentiment_analysis_invalid_input(client: FlaskClient, text):
     """Test the sentiment analysis with invalid input."""
-    response = client.post("/api/v1/sentiment-analysis", json={"text": text})
+    response = client.post(
+        "/api/v1/sentiment-analysis", json={"text": text, "process_text": True}
+    )
     json_data = response.get_json()
 
     assert response.status_code == 422 and "error" in json_data
@@ -41,7 +44,8 @@ def test_sentiment_analysis_invalid_input(client: FlaskClient, text):
 def test_sentiment_analysis_internal_error(client: FlaskClient):
     """Test the sentiment analysis to ensure it handles internal errors."""
     response = client.post(
-        "/api/v1/sentiment-analysis", json={"invalid_field": "Some text"}
+        "/api/v1/sentiment-analysis",
+        json={"invalid_field": "Some text", "process_text": True},
     )
     json_data = response.get_json()
 
